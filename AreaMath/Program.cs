ef85f94e8239847114d0f8f9f3cd3057a0e6a14a
Isaac Weiland <iweiland7777@gmail.com>
2024-08-11 19:47:51 -0400

    added trapezoid, allowed use of 1 decimal point in calcuation

--- AreaMath/Program.cs
+++ AreaMath/Program.cs
@@ -11,32 +11,43 @@ class Program
             case "square":
             {
                 Console.WriteLine("Enter side length.");
-                int s = int.Parse(Console.ReadLine());
+                double s = double.Parse(Console.ReadLine());
                 Console.WriteLine($"Area of this square is { s * s }");
                 break;
             }
             case "rectangle":
             {
                 Console.WriteLine("Enter length.");
-                int l = int.Parse(Console.ReadLine());
+                double l = double.Parse(Console.ReadLine());
                 Console.WriteLine("Enter Width.");
-                int w = int.Parse(Console.ReadLine());
+                double w = double.Parse(Console.ReadLine());
                 Console.WriteLine($"Area of this rectangle is {l * w}.");
                 break;
             }
             case "triangle":
             {
-                Console.WriteLine("Please enter you base length.");
-                int b = int.Parse((Console.ReadLine()));
-                Console.WriteLine("Please enter your height length.");
-                int h = int.Parse(Console.ReadLine());
+                Console.WriteLine("Enter you base length.");
+                double b = double.Parse((Console.ReadLine()));
+                Console.WriteLine("Enter your height length.");
+                double h = double.Parse(Console.ReadLine());
                 Console.WriteLine($"Area of your triangle is { .5 * b * h}");
+                break;
+            }
+            case "trapezoid":
+            {
+                Console.WriteLine("Enter the length of your first base.");
+                double l1 = double.Parse(Console.ReadLine());
+                Console.WriteLine("Enter the length of your second base.");
+                double l2 = double.Parse(Console.ReadLine());
+                Console.WriteLine("Enter the height.");
+                double h = double.Parse(Console.ReadLine());
+                Console.WriteLine($"The area of your trapezoid is { (l1 + l2) * h / 2}");
                 break;
             }
             default:
             {
                 Console.WriteLine("Invalid input or unaccepted polygon.\nList of accepted polygons are:\n" +
-                                  "Square\nRectangle\nTriangle");
+                                  "Square\nRectangle\nTriangle\nTrapezoid");
                 break;
             }
         }
