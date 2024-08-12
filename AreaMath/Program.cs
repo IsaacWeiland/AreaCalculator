ecbabd743fd89b347d0efac60906ddffce2a05ec
Isaac Weiland <iweiland7777@gmail.com>
2024-08-12 14:56:20 -0400

    made code fit in with exercise requirements

--- AreaMath/Program.cs
+++ AreaMath/Program.cs
@@ -4,36 +4,37 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("What polygon would you like to calculate?");
+        Console.WriteLine("What polygon would you like to calculate?");//circles are polygons with infinite sides, okay
         string polygon = Console.ReadLine();
         switch (polygon)
         {
             case "square":
             {
                 Console.WriteLine("Enter side length.");
-                double s = double.Parse(Console.ReadLine());
-                Console.WriteLine($"Area of this square is { s * s }");
+                double side = double.Parse(Console.ReadLine());
+                Console.WriteLine($"Area of this square is { SquareArea(side) }");
                 break;
             }
             case "rectangle":
             {
                 Console.WriteLine("Enter length.");
-                double l = double.Parse(Console.ReadLine());
+                double length = double.Parse(Console.ReadLine());
                 Console.WriteLine("Enter Width.");
-                double w = double.Parse(Console.ReadLine());
-                Console.WriteLine($"Area of this rectangle is {l * w}.");
+                double width = double.Parse(Console.ReadLine());
+                Console.WriteLine($"Area of this rectangle is {RectangleArea(length, width)}.");
                 break;
             }
             case "triangle":
             {
                 Console.WriteLine("Enter you base length.");
-                double b = double.Parse((Console.ReadLine()));
+                double baseLength = double.Parse((Console.ReadLine()));
                 Console.WriteLine("Enter your height length.");
-                double h = double.Parse(Console.ReadLine());
-                Console.WriteLine($"Area of your triangle is { .5 * b * h}");
+                double height = double.Parse(Console.ReadLine());
+                Console.WriteLine($"Area of your triangle is { TriangleArea(baseLength, height)}");
                 break;
             }
-            case "trapezoid":
+            case "trapezoid": //to whoever is grading this, you can just ignore this. I made it before I knew this was
+                //an assignment, and I'm too proud to delete this code but too lazy to change it.
             {
                 Console.WriteLine("Enter the length of your first base.");
                 double l1 = double.Parse(Console.ReadLine());
@@ -44,12 +45,41 @@ class Program
                 Console.WriteLine($"The area of your trapezoid is { (l1 + l2) * h / 2}");
                 break;
             }
+            case "circle":
+            {
+                Console.WriteLine("Enter the area of your circle.");
+                double radius = int.Parse(Console.ReadLine());
+                Console.WriteLine($"The area of your circle is {CircleArea(radius)}");
+                break;
+            }
             default:
             {
                 Console.WriteLine("Invalid input or unaccepted polygon.\nList of accepted polygons are:\n" +
-                                  "Square\nRectangle\nTriangle\nTrapezoid");
+                                  "Circle\nSquare\nRectangle\nTriangle\nTrapezoid");
                 break;
             }
+            
         }
+        
+    }
+    public static double CircleArea(double radius)
+    {
+        double area = Math.PI * Math.Pow(radius, 2);
+        return area;
+    }
+
+    public static double SquareArea(double side)
+    {
+        return side * side;
+    }
+
+    public static double RectangleArea(double length, double width)
+    {
+        return length * width;
+    }
+
+    public static double TriangleArea(double baseLength, double height)
+    {
+        return .5 * baseLength * height;
     }
 }
